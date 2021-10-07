db2fa828d5f0e7cf9081429f82ed3001365fb84c
mohammad <vaniladra@gmail.com>
2021-10-08 01:46:51 +0330

    created a session object in servlet

--- src/main/java/ir/maktab/AddServlet.java
+++ src/main/java/ir/maktab/AddServlet.java
@@ -5,28 +5,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         String n11 = req.getParameter("n1");
         String n21 = req.getParameter("n2");
+        HttpSession session = req.getSession();
         System.out.println(n11 +" "+n21);
         int n1 = Integer.parseInt(n11);
         System.out.println(n1);
         int n2 = Integer.parseInt(n21);
         System.out.println(n2);
         n1 = n1+n2;
-//
+        session.setAttribute("k",n1);
+
         PrintWriter writer = resp.getWriter();
         writer.println("sum is "+n1);
-        req.setAttribute("k",n1);
-        //  INSIDE THE METHOD PARENTHESIS WE GIVE
-        //  SERVLET ROUTE
-        RequestDispatcher sq = req.getRequestDispatcher("sq");
-        sq.forward(req,resp);
+
+        resp.sendRedirect("sq");
 
     }
 }
--- src/main/java/ir/maktab/SqServlet.java
+++ src/main/java/ir/maktab/SqServlet.java
@@ -4,14 +4,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SqServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
-//        String k = req.getParameter("k");
-        Object k = req.getAttribute("k");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        HttpSession session = req.getSession();
+        Object k = session.getAttribute("k");
         int i = Integer.parseInt(k.toString());
         i = i *i;
         PrintWriter writer = resp.getWriter();
